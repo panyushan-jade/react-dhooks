91a5e62bd0909e91296dc513c37f8767f1786e11
panyushan <1193345664@qq.com>
2023-03-06 23:26:30 +0800

    pref：更换logo

--- .umirc.ts
+++ .umirc.ts
@@ -4,9 +4,8 @@ export default defineConfig({
   base: '/dHooks',
   title: 'react-dhooks',
   publicPath: '/dHooks/',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon:'/icon.svg',
+  logo: '/icon.svg',
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
